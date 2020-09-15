aeab53e3259d2b3b1bc893a32ecfbcbf4fd1601a
alam1008 <angela.lam@utah.edu>
2020-09-15 00:24:36 -0600

    Build a simple calculator app

---
+++ calc.py
@@ -0,0 +1,21 @@
+Python 3.8.5 (v3.8.5:580fbb018f, Jul 20 2020, 12:11:27) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+# This Python code acts as a calculator for two integers.
+
+# Enter the first number
+>>> x = int(input('Enter the first number: '))
+Enter a number: 3
+
+# Enter the second number
+>>> y = int(input('Enter the second number: '))
+Enter another number: 5
+
+# Add the integers together
+>>> result = x + y
+
+# Display the results
+>>> print(result)
+8
+
